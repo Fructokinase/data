9abebaa3af6fb0f6140595e30f47e5a2a96def1d
Alex Chen <alexyfchen@google.com>
2023-03-29 16:42:47 -0700

    SCHEDULES=scripts/covid19_india/cases_count_states_data:covid19IndiaCasesCountStatesData

--- import-automation/executor/gke/configure_gke.sh
+++ import-automation/executor/gke/configure_gke.sh
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #!/bin/bash
+#
 gcloud container clusters get-credentials \
   datacommons-us-central1 \
   --region us-central1 \
